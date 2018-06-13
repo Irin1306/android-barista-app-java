5ac19c779fa9f73bc83c3a8e3dc13d4519180586
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2018-06-13 21:52:00 +0300

    add fix

--- app/src/main/java/io/brainyapps/barista/ui/drinks/DrinksListAdapter.java
+++ app/src/main/java/io/brainyapps/barista/ui/drinks/DrinksListAdapter.java
@@ -3,7 +3,6 @@ package io.brainyapps.barista.ui.drinks;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -72,20 +71,8 @@ public class DrinksListAdapter extends
 
     @Override
     public void addFirstElement(Drink drink) {
-        //mDrinks.add(0, drink);
-        //notifyItemInserted(2);
-        boolean fuckingCycle = true;
-        long z = 0;
-
-        for (int i = 0; i < 100; i++) {
-            i = 0;
-            z++;
-            Log.i("tttttqweqweqwe", "z = " + z);
-            Drink drink1 = new Drink();
-            drink1.setName("qweqwe");
-            mDrinks.add(2, drink1);
-            notifyItemInserted(2);
-        }
+        mDrinks.add(0, drink);
+        notifyItemInserted(0);
     }
 
     // ViewHolder
