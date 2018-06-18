99468986ab29705f8fcbb0cdeead517253136ac3
Serhii Pokrovskyi <10260795+serhii-pokrovskyi@users.noreply.github.com>
2018-06-18 21:11:26 +0300

    application class added

---
+++ app/src/main/java/io/brainyapps/barista/App.java
@@ -0,0 +1,11 @@
+package io.brainyapps.barista;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
